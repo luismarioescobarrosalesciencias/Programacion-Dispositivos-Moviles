0e49e4e26aab1b904ae54668275ebcf0a3e2c912
Luis Mario <luismescobar@ciencias.unam.mx>
2023-05-08 18:33:01 -0600

    navegation drawer

--- Tareas/Tarea3Barras/Tarea2Menus/app/src/main/java/com/DispositivosMoviles2023/myapplication/MainActivity.java
+++ Tareas/Tarea3Barras/Tarea2Menus/app/src/main/java/com/DispositivosMoviles2023/myapplication/MainActivity.java
@@ -12,6 +12,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
 /**
  * Nombre de los integrantes:
  *
@@ -23,31 +31,51 @@ public class MainActivity extends AppCompatActivity {
 
     //private AppBarConfiguration appBarConfiguration;
     //private ActivityMainBinding binding;
+    private String[] mMenuSections;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        //setContentView(binding.getRoot());
-
-        //Hay que descomentar la vista que queramos ver y comentar las demas
-
         setContentView(R.layout.fragment_first); //CATALOGO
+        mMenuSections = getResources().getStringArray(com.DispositivosMoviles2023.myapplication.R.array. menu_items);
+        mDrawerLayout = (DrawerLayout) findViewById(com.DispositivosMoviles2023.myapplication.R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(com.DispositivosMoviles2023.myapplication.R.id.left_drawer);
+        mDrawerList.setAdapter( new ArrayAdapter<String>( this, com.DispositivosMoviles2023.myapplication.R.layout. left_drawer, mMenuSections));
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                com.DispositivosMoviles2023.myapplication.R.string.app_name,  /* "open drawer" description */
+                com.DispositivosMoviles2023.myapplication.R.string.app_name  /* "close drawer" description */
+        ){
+            public void onDrawerClosed(View view) {
+                Log.d("HomeActivity", "onDrawerClosed");
+            }
 
-        //setContentView(R.layout.fragment_second); //BUSQUEDA
+            public void onDrawerOpened(View drawerView) {
+                Log.d("HomeActivity", "onDrawerOpened");
+            }
+        };
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        //setContentView(R.layout.fragment_third); //LOGIN
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        //setContentView(R.layout.fragment_four); //PORTADA
-
-        //setSupportActionBar(binding.toolbar);
-
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
     }
+    private class DrawerItemClickListener implements
+            ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent,
+                                View view, int position, long id) {
+            Log.d("HomeActivity",
+                    (String) parent.getAdapter().getItem(position));
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,8 +87,14 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
-
+        if (id == com.DispositivosMoviles2023.myapplication.R.id. action_settings) {
+            Log. d("HomeActivity", "action_settings");
+            return true;
+        }
         switch (id) {
             case R.id.action_settings :
                 Log.d("HomeActivity", "Acciones");
@@ -77,8 +111,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_preferences:
                 Log.d("HomeActivity", "Preferencias");
                 return true;
-        }
 
+        }
 
         ActionBar actionBar = getSupportActionBar();
         assert false;
